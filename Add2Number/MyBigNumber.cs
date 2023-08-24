24b367fa2256f91007712a83a51218550c6f748c
Le Viet Sanh <levietsanh123@gmail.com>
2023-08-24 10:17:40 +0700

    Update MyBigNumber

--- Add2Number/MyBigNumber.cs
+++ Add2Number/MyBigNumber.cs
@@ -15,16 +15,16 @@ namespace Add2Number
             int lengthMax = num1Length > num2Length ? num1Length : num2Length;
             String result = String.Empty;
             int temp = 0;
-            int num1I, num2I; // num1I luu lai vi tri ky tu cua chuoi num 1 (theo chieu phai sang trai),num2I tuong tu
+            int indexNum1, indexNum2; // num1I luu lai vi tri ky tu cua chuoi num 1 (theo chieu phai sang trai),num2I tuong tu
             char c1, c2; // c1 lay ky tu, c2 tuong tu
             int digit1, digit2; // digit1 chuyen ky tu thanh so
             int mem = 0;
             for( int i=0; i< lengthMax; i++)
             {
-                num1I = num1Length - i - 1;
-                num2I = num2Length - i - 1;
-                c1 = (num1I >= 0) ? num1[num1I] : '0';
-                c2 = (num2I >= 0) ? num2[num2I] : '0';
+                indexNum1 = num1Length - i - 1;
+                indexNum2 = num2Length - i - 1;
+                c1 = (indexNum1 >= 0) ? num1[indexNum1] : '0';
+                c2 = (indexNum2 >= 0) ? num2[indexNum2] : '0';
                 digit1 = c1 - '0';
                 digit2 = c2 - '0';
 
